839fe2be646ea0080eb41ccfa2262a6ce803d35d
Volodymyr Butko <volodymyr_butko@epam.com>
2023-04-30 21:57:57 +0300

    Added basic messages handling in bot

--- cmd/bot.go
+++ cmd/bot.go
@@ -1,19 +1,25 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
 )
+
+var TelegramToken = os.Getenv("TELEGRAM_TOKEN")
 
 // botCmd represents the bot command
 var botCmd = &cobra.Command{
-	Use:   "bot",
-	Short: "A brief description of your command",
+	Use:     "bot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +27,33 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bot called")
+		fmt.Printf("bot %s started", appVersion)
+
+		bot, err := telebot.NewBot(telebot.Settings{
+			URL:   "",
+			Token: TelegramToken,
+			Poller: &telebot.LongPoller{
+				Timeout: 10 * time.Second,
+			},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELEGRAM_TOKEN")
+		}
+
+		bot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s", appVersion))
+			}
+
+			return err
+		})
+
+		bot.Start()
 	},
 }
 
